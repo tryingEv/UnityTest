4772166102edb7ecd49697c8a83eaaa6feeffeef
CipherAnt <tryingEv@Outlook.com>
2019-03-02 19:22:02 +0800

    collation project files

--- Project/Assets/Editor/EditorTest.cs
+++ Project/Assets/Editor/EditorTest.cs
@@ -1,11 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using NUnit.Framework.Internal;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
-using UnityEngine.Experimental.UIElements;
 
-public class EditorTest 
+public class EditorTest
 {
     [MenuItem("GameObject/Test MenuItem in GameObject Menu", false, 22)]
     public static void TestMenuItem()
@@ -14,7 +10,7 @@ public class EditorTest
     }
 
     /// <summary>
-    /// 在Inspector面板中Transform组件右键鼠标将会显示该菜单，如果需要在其他组件中显示，则只需要将Transform组件换成其他组件
+    ///     在Inspector面板中Transform组件右键鼠标将会显示该菜单，如果需要在其他组件中显示，则只需要将Transform组件换成其他组件
     /// </summary>
     [MenuItem("CONTEXT/Transform/RightClickOnTransformComponet")]
     public static void TestMenuItemInInspectorComponetRightClick()
@@ -23,18 +19,18 @@ public class EditorTest
     }
 
     /// <summary>
-    /// MenuItem的第二个参数为true时，表示在该函数返回true时显示，同时需要有一个对应的函数在何时不显示，如TestMenuItemSecondPararmentHide
+    ///     MenuItem的第二个参数为true时，表示在该函数返回true时显示，同时需要有一个对应的函数在何时不显示，如TestMenuItemSecondPararmentHide
     /// </summary>
     /// <returns></returns>
     [MenuItem("Assets/TestMenuItemSecondPararment", true, 11)]
     public static bool TestMenuItemSecondPararmentShow()
     {
-        return (null != Selection.activeObject);
+        return null != Selection.activeObject;
     }
-    
+
     [MenuItem("Assets/TestMenuItemSecondPararment", false, 11)]
     public static bool TestMenuItemSecondPararmentHide()
     {
-        return (null == Selection.activeObject);
+        return null == Selection.activeObject;
     }
 }
